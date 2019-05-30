1cdba4ea65aceda0ef872c089629c2855a8d7e08
Justin Anderson <jander@microsoft.com>
2019-05-30 16:33:45 -0700

    Return effective flags from GetLoggingFlags

--- src/InstrumentationEngine.Lib/LoggerService.cpp
+++ src/InstrumentationEngine.Lib/LoggerService.cpp
@@ -64,7 +64,12 @@ HRESULT CLoggerService::GetLoggingFlags(_Out_ LoggingFlags* pLoggingFlags)
 
     CCriticalSectionHolder holder(&m_cs);
 
-    *pLoggingFlags = m_defaultFlags;
+    // Intentionally returning the effective flags instead of the default flags
+    // (ones set by default or by SetLoggingFlags). The GetLoggingFlags method
+    // is primarily meant for consumers external of the Instrumentation Engine
+    // assembly to determine which flags are supported rather than literally
+    // reporting what the default was or what was set by SetLoggingFlags.
+    *pLoggingFlags = m_effectiveFlags;
 
     return S_OK;
 }
