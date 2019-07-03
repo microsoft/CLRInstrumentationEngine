d2525cd4807bd3924c2ad382fa9a05929cdc066e
WilliamXieMSFT <willxie@microsoft.com>
2019-07-03 14:22:31 -0700

    Fixed with suggestions

--- verifyCopyrightHeaders.ps1
+++ verifyCopyrightHeaders.ps1
@@ -9,10 +9,6 @@ $mitLicenseHeader = 'Licensed under the MIT License.'
 $xmlHeader        = '<?xml'
 $shellHeader        = '#!/bin/bash'
 
-$resultFile = "verifyCopyrightHeadersResult.log"
-Write-Verbose "Generating $resultFile" 
-New-Item $resultFile -Force | Out-Null
-
 $excludedDirs =  @(
     # generated
     '.vs'
@@ -74,17 +70,17 @@ Write-Verbose ""
 Write-Verbose "Checking files under directory '$PSScriptRoot' ..."
 Write-Verbose "Non-compliant files will be stored in $resultFile"
 
-Get-ChildItem -Path $PSScriptRoot -Recurse -File | 
-    Where-Object { $_.PSParentPath -notmatch "\\$([String]::Join('|', $excludedDirs))" } | 
-    Where-Object { !$excludedFiles.Contains($_.Name) } |
-    Where-Object { !$excludedExtensions.Contains($_.Extension) } | 
-    ForEach-Object { 
+Get-ChildItem -Path $PSScriptRoot -Recurse -File |
+    Where-Object { $_.PSParentPath -notmatch "\\$([String]::Join('|', $excludedDirs))" } |
+    Where-Object { -not $excludedFiles.Contains($_.Name) } |
+    Where-Object { -not $excludedExtensions.Contains($_.Extension) } |
+    ForEach-Object {
         $lines = Get-Content $_.FullName -First 3
         $copyrightIndex = 0
         $mitLicenseIndex = 1
         if ($lines)
         {
-            if ($lines[0].ToString().Contains($xmlHeader) -or 
+            if ($lines[0].ToString().Contains($xmlHeader) -or
                 $lines[0].ToString().Contains($shellHeader))
             {
                 $copyrightIndex = 1
@@ -94,21 +90,13 @@ Get-ChildItem -Path $PSScriptRoot -Recurse -File |
             if (!$lines[$copyrightIndex].ToString().Contains($copyrightHeader) -or
                 !$lines[$mitLicenseIndex].ToString().Contains($mitLicenseHeader))
             {
-                "Could not find OSS header in $($_.FullName)" > $resultFile
+                Write-Error "$($_.FullName) is missing OSS header."
             }
         }
         else
         {
-            $msg = "$($_.FullName) is empty. Consider modifying this script to exclude the file."
-            Write-Warning $msg > $resultFile
+            Write-Error "$($_.FullName) is empty."
         }
     }
 
-if (Get-Content $resultFile)
-{
-    Write-Error "See '$resultFile' for the list of files to fix."
-}
-else
-{
-    Write-Verbose "Done."
-}
+Write-Verbose "Done."
