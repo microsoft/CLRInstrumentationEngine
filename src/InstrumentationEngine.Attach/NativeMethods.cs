640e35a0a3d7219ff5a6b3eff118364dd529ae6c
Justin Anderson <jander-msft@users.noreply.github.com>
2020-02-26 14:38:49 -0800

    Initial attach executable (#212)
    * Initial attach executable.

---
+++ src/InstrumentationEngine.Attach/NativeMethods.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.InstrumentationEngine
+{
+    internal static class NativeMethods
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool IsWow64Process([In] IntPtr hProcess, [Out] out bool wow64Process);
+    }
+}
