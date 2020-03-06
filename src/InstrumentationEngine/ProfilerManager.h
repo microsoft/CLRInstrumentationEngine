e7ccbd8121f5b2a33f71f5c607272bd05c84db44
Justin Anderson <jander@microsoft.com>
2020-03-06 12:48:41 -0800

    PR Feedback

--- src/InstrumentationEngine/ProfilerManager.h
+++ src/InstrumentationEngine/ProfilerManager.h
@@ -22,8 +22,6 @@ namespace MicrosoftInstrumentationEngine
 
     const GUID CLSID_CProfilerManager = { 0x324F817A, 0x7420, 0x4E6D,{ 0xB3, 0xC1, 0x14, 0x3f, 0xBE, 0xD6, 0xD8, 0x55 } };
 
-    const size_t WCharSizeInBytes = sizeof(WCHAR) / sizeof(BYTE);
-
     // This abstract class should be updated with new IProfilerManager interfaces.
     // Both CProfilerManager and CProfilerManagerForInstrumentationMethod inherit this class.
     class IProfilerManagerContract :
