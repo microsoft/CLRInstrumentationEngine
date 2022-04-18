0e683f2a5e8d990f72790ea35acc9685f97b8cba
Del Myers <delmyers@microsoft.com>
2022-04-18 14:09:58 -0700

    Add linux string cleanup apis
    Use dlsym/dlopen with NOLOAD
    Everything compiles in Linux. Unit tests pass.

--- src/Tests/InstrEngineTests/NaglerInstrumentationMethod/InstrumentationEngineApi.cpp
+++ src/Tests/InstrEngineTests/NaglerInstrumentationMethod/InstrumentationEngineApi.cpp
@@ -5,6 +5,21 @@
 #include "InstrumentationEngineApi.h"
 #include "../Common.Headers/InitOnce.h"
 
+#if defined(PLATFORM_UNIX)
+
+#include <dlfcn.h>
+
+#if (defined(_M_X64) || defined(_M_ARM64) || defined(_M_AMD64))
+const char InstrumentationEngineModule[] = "MicrosoftInstrumentationEngine_x64.so";
+#else
+#error "Platform Not Supported"
+#endif
+
+#define MOD_TYPE void*
+#define MOD_LOAD(_N) (dlopen(_N, RTLD_NOLOAD))
+#define SYM_LOAD(_M, _S) (dlsym(_M, _S))
+
+#else // !PLATFORM_UNIX
 #include <Windows.h>
 
 #if defined(X86)
@@ -15,42 +30,48 @@ const WCHAR InstrumentationEngineModule[] = _T("MicrosoftInstrumentationEngine_x
 #error "Platform Not Supported"
 #endif
 
+#define MOD_TYPE HMODULE
+#define MOD_LOAD(_N) (GetModuleHandle(_N))
+#define SYM_LOAD(_M, _S) (GetProcAddress(_M, _S))
+
+#endif
+
 using namespace std;
 
-class ApiFunctions
+namespace InstrumentationEngineApi
 {
-public:
-    using TFreeString = HRESULT(STDMETHODCALLTYPE*)(BSTR bstr);
-    TFreeString _InstrumentationEngineFreeString = nullptr;
-    HRESULT Initialize();
-};
 
-static ApiFunctions g_apiFunctions;
-static CInitOnce g_initializer([] { return g_apiFunctions.Initialize(); });
-
-
-HRESULT InstrumentationEngineApi::FreeString(BSTR bstr)
-{
-    HRESULT hr;
-    IfFailRet(g_initializer.Get());
-    return g_apiFunctions._InstrumentationEngineFreeString(bstr);
-}
-
-HRESULT ApiFunctions::Initialize()
-{
-    // Todo: this will have to be done differently for non Windows platforms.
-    HMODULE mod = GetModuleHandle(InstrumentationEngineModule);
-    if (mod == nullptr)
+    class ApiFunctions
     {
-        return E_NOTIMPL;
-    }
+    public:
+        using TFreeString = HRESULT(STDMETHODCALLTYPE*)(BSTR bstr);
+        TFreeString _InstrumentationEngineFreeString = nullptr;
+        
+        HRESULT Initialize()
+        {
+            MOD_TYPE mod = MOD_LOAD(InstrumentationEngineModule);
+            if (mod == nullptr)
+            {
+                return E_NOTIMPL;
+            }
 
-    _InstrumentationEngineFreeString = (TFreeString)GetProcAddress(mod, "InstrumentationEngineFreeString");
-    if (_InstrumentationEngineFreeString == nullptr)
+            _InstrumentationEngineFreeString = (TFreeString)SYM_LOAD(mod, "InstrumentationEngineFreeString");
+            if (_InstrumentationEngineFreeString == nullptr)
+            {
+                return E_NOTIMPL;
+            }
+
+            return S_OK;
+        }
+    };
+
+    static InstrumentationEngineApi::ApiFunctions g_apiFunctions;
+    static CInitOnce g_initializer([] { return g_apiFunctions.Initialize(); });
+
+    HRESULT FreeString(BSTR bstr)
     {
-        return E_NOTIMPL;
+        HRESULT hr;
+        IfFailRet(g_initializer.Get());
+        return g_apiFunctions._InstrumentationEngineFreeString(bstr);
     }
-
-    return S_OK;
 }
-
