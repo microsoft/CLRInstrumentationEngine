bce583d3d9bcb20aa1982fe6147d3c1114eb122b
kkeirstead <kkeirstead@microsoft.com>
2022-05-23 14:47:31 -0700

    Added C# methods that simulate what Aaron's validate methods do; not currently functional

---
+++ src/Tests/InstrEngineTests/InstrEngineTests/EmbeddedResources/ValidateByRefLikeInvalidCSharp.cs
@@ -0,0 +1,184 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using ByRefLikeInvalidCSharp;
+
+namespace Validate
+{
+    class Program
+    {
+        static int error = 0;
+
+        static int Main(string[] args)
+        {
+            Validate_TypeLoad();
+            Validate_Casting_Scenarios();
+            Validate_RecognizedOpCodeSequences_Scenarios();
+            Validate_InvalidOpCode_Scenarios();
+            Validate_Inlining_Behavior();
+
+            return error; // If everything works as expected, error should be 0
+        }
+
+        private static void Validate_TypeLoad()
+        {
+            Console.WriteLine($"{nameof(Validate_TypeLoad)}...");
+
+            Console.WriteLine($" -- Instantiate: {Exec.GenericClass()}");
+            Console.WriteLine($" -- Instantiate: {Exec.GenericInterface()}");
+            Console.WriteLine($" -- Instantiate: {Exec.GenericValueType()}");
+            Console.WriteLine($" -- Instantiate: {Exec.GenericByRefLike()}");
+
+            int errorCount = 0;
+            const int expectedErrorCount = 4;
+
+            try
+            {
+                Exec.GenericClass_Invalid();
+            }
+            catch (TypeLoadException)
+            {
+                errorCount += 1;
+            }
+
+            try
+            {
+                Exec.GenericInterface_Invalid();
+            }
+            catch (TypeLoadException)
+            {
+                errorCount += 1;
+            }
+
+            try
+            {
+                Exec.GenericValueType_Invalid();
+            }
+            catch (TypeLoadException)
+            {
+                errorCount += 1;
+            }
+
+            try
+            {
+                Exec.GenericByRefLike_Invalid();
+            }
+            catch (TypeLoadException)
+            {
+                errorCount += 1;
+            }
+
+            error = errorCount == expectedErrorCount ? error : -1;
+        }
+
+        private static void Validate_Casting_Scenarios()
+        {
+            Console.WriteLine($"{nameof(Validate_Casting_Scenarios)}...");
+
+            // Opcodes that can handle cases naturally for ByRefLike types should fail.
+            // Since ByRefLike types can never be boxed, it stands to reason attempting
+            // to cast an object to a ByRefLike type will always return null or throw an
+            // appropriate exception.
+
+            error = !Exec.InstanceOfT(new object()) ? error : -1;
+
+            int errorCount = 0;
+            const int expectedErrorCount = 2;
+
+            try
+            {
+                Exec.CastToT(new object());
+            }
+            catch (InvalidCastException)
+            {
+                errorCount += 1;
+            }
+
+            try
+            {
+                Exec.UnboxToT(new object());
+            }
+            catch (InvalidCastException)
+            {
+                errorCount += 1;
+            }
+
+            error = errorCount == expectedErrorCount ? error : -1;
+        }
+
+        private static void Validate_RecognizedOpCodeSequences_Scenarios()
+        {
+            Console.WriteLine($"{nameof(Validate_RecognizedOpCodeSequences_Scenarios)}...");
+
+            error = Exec.BoxUnboxAny() ? error : -1;
+            error = Exec.BoxBranch() ? error : -1;
+            error = Exec.BoxIsinstUnboxAny() ? error : -1;
+            error = Exec.BoxIsinstBranch() ? error : -1;
+        }
+
+        private static void Validate_InvalidOpCode_Scenarios()
+        {
+            Console.WriteLine($"{nameof(Validate_InvalidOpCode_Scenarios)}...");
+
+            // These methods uses opcodes that are not able to handle ByRefLike type operands.
+            // The TypeLoader prevents these invalid types from being constructed. We rely on
+            // the failure to construct these invalid types to block opcode usage.
+            int errorCount = 0;
+            const int expectedErrorCount = 4;
+
+            try
+            {
+                Exec.AllocArrayOfT_Invalid();
+            }
+            catch (TypeLoadException)
+            {
+                errorCount += 1;
+            }
+
+            try
+            {
+                Exec.AllocMultiDimArrayOfT_Invalid();
+            }
+            catch (TypeLoadException)
+            {
+                errorCount += 1;
+            }
+
+            try
+            {
+                Exec.GenericClassWithStaticField_Invalid();
+            }
+            catch (TypeLoadException)
+            {
+                errorCount += 1;
+            }
+
+            // Test that explicitly tries to box a ByRefLike type.
+            try
+            {
+                Exec.BoxAsObject();
+            }
+            catch (InvalidProgramException)
+            {
+                errorCount += 1;
+            }
+
+            error = errorCount == expectedErrorCount ? error : -1;
+
+        }
+
+        private static void Validate_Inlining_Behavior()
+        {
+            Console.WriteLine($"{nameof(Validate_Inlining_Behavior)}...");
+
+            // The call here is to ensure that an inlinable method that isn't called
+            // due to a branch, see input argument, doesn't break the compilation of the
+            // rest of method that is executed.
+            error = Exec.TypeLoadExceptionAvoidsInline(false) ? error : -1;
+        }
+    }
+}
---
+++ src/Tests/InstrEngineTests/InstrEngineTests/EmbeddedResources/ValidateInvalidCSharp.cs
@@ -0,0 +1,183 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using InvalidCSharp;
+
+namespace Validate
+{
+    class Program
+    {
+        static int error = 0;
+
+        static int Main(string[] args)
+        {
+            Validate_Invalid_RefField_Fails();
+            Validate_RefStructWithRefField_Load();
+            Validate_Create_RefField();
+            Validate_Create_RefStructField();
+            Validate_Create_TypedReferenceRefField();
+
+            return error; // If everything works as expected, error should be 0
+        }
+
+        private static void Validate_Invalid_RefField_Fails()
+        {
+            Console.WriteLine($"{nameof(Validate_Invalid_RefField_Fails)}...");
+
+            int errorCount = 0;
+            const int expectedErrorCount = 6;
+
+            try
+            {
+                var t = typeof(InvalidStructWithRefField);
+            }
+            catch (TypeLoadException)
+            {
+                errorCount += 1;
+            }
+
+            try
+            {
+                var t = typeof(InvalidRefFieldAlignment);
+            }
+            catch (TypeLoadException)
+            {
+                errorCount += 1;
+            }
+
+            try
+            {
+                var t = typeof(InvalidObjectRefRefFieldOverlap);
+            }
+            catch (TypeLoadException)
+            {
+                errorCount += 1;
+            }
+
+            try
+            {
+                var t = new IntPtrRefFieldOverlap()
+                {
+                    Field = IntPtr.Zero
+                };
+
+                var tFieldString = t.Field.ToString();
+            }
+            catch (TypeLoadException)
+            {
+                errorCount += 1;
+            }
+
+            try
+            {
+                var t = new IntPtrOverlapWithInnerFieldType()
+                {
+                    Field = IntPtr.Zero
+                };
+
+                ref var i = ref t.Invalid;
+                var size iSize = i.Size;
+            }
+            catch (TypeLoadException)
+            {
+                errorCount += 1;
+            }
+
+            try
+            {
+                var t = new Explicit()
+                {
+                    Guid = Guid.NewGuid()
+                };
+                var tLength = t.Bytes.Length;
+            }
+            catch (TypeLoadException)
+            {
+                errorCount += 1;
+            }
+
+            error = errorCount == expectedErrorCount ? error : -1;
+        }
+
+        private static void Validate_RefStructWithRefField_Load()
+        {
+            Console.WriteLine($"{nameof(Validate_RefStructWithRefField_Load)}...");
+            var t = typeof(WithRefField);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Create_RefField_Worker(string str, int depth)
+        {
+            if (depth == 5)
+            {
+                return;
+            }
+
+            WithRefField s = new(ref str);
+            string newStr = new(str);
+
+            Create_RefField_Worker(str + $" {depth}", depth + 1);
+
+            error = !s.ConfirmFieldInstance(newStr) ? error : -1;
+            error = s.ConfirmFieldInstance(str) ? error : -1;
+        }
+
+        private static void Validate_Create_RefField()
+        {
+            var str = nameof(Validate_Create_RefField);
+            Console.WriteLine($"{str}...");
+            Create_RefField_Worker(str, 1);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Create_RefStructField_Worker(string str, int depth)
+        {
+            if (depth == 5)
+            {
+                return;
+            }
+
+            WithRefField s = new(ref str);
+            WithRefStructField t = new(ref s);
+
+            Create_RefStructField_Worker(str + $" {depth}", depth + 1);
+
+            error = t.ConfirmFieldInstance(ref s) ? error : -1;
+        }
+
+        private static void Validate_Create_RefStructField()
+        {
+            var str = nameof(Validate_Create_RefStructField);
+            Console.WriteLine($"{str}...");
+            Create_RefStructField_Worker(str, 1);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Create_TypedReferenceRefField_Worker(Validate v, int depth)
+        {
+            if (depth == 5)
+            {
+                return;
+            }
+
+            WithTypedReferenceField<Validate> s = new(ref v);
+
+            Create_TypedReferenceRefField_Worker(v, depth + 1);
+
+            error = typeof(Validate) == s.GetFieldType() ? error : -1;
+            error = s.ConfirmFieldInstance(v) ? error : -1;
+        }
+
+        private static void Validate_Create_TypedReferenceRefField()
+        {
+            Console.WriteLine($"{nameof(Validate_Create_TypedReferenceRefField)}...");
+
+            Validate v = new();
+            Create_TypedReferenceRefField_Worker(v, 1);
+        }
+    }
+}
